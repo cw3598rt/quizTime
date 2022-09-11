cb89bc6734c1f0c828e725dc9d5ac2e4722c9b45
김지수 <kimjisu@gimjisuui-MacBookAir.local>
2022-09-11 21:04:59 +0900

    [추가]테스트 작성 시도

---
+++ cypress/e2e/check/index.cy.js
@@ -0,0 +1,14 @@
+it("퀴즈페이지로 이동", () => {
+  cy.visit("http://localhost:3000");
+  cy.get("div").click({ multiple: true });
+  cy.get("section");
+});
+it("퀴즈디테일페이지", () => {
+  cy.visit(`http://localhost:3000/quiz/detail/25}`);
+  cy.get("button").click();
+  cy.get("div");
+  cy.get("section");
+  cy.get("div");
+  cy.get("h3");
+  cy.get("pre");
+});
